e72a2c93dedab4a41c1b57be376a654aeb4a6aeb
mirajhad <mirajhadish48@gmail.com>
2024-06-12 02:48:03 +0530

    logger integrated in app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express"
 import cors from "cors"
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
+import logger from './utils/logger.js';
+import morgan from 'morgan';
+
+const morganFormat = ':method :url :status :response-time ms';
 const app = express()
 
 
@@ -14,6 +18,20 @@ app.use(express.urlencoded({extended: true, limit: "100kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+app.use(morgan(morganFormat, {
+  stream: {
+    write: (message) => {
+      const logObject = {
+        method: message.split(' ')[0],
+        url: message.split(' ')[1],
+        status: message.split(' ')[2],
+        responseTime: message.split(' ')[3],
+
+      };
+      logger.info(JSON.stringify(logObject));
+    }
+  }
+}));
 
 
 // routes import
